4d01cfa416ced700c83d66f0c5ed344749262d9a
vsoldera <victorsolderalf@gmail.com>
2025-01-28 20:10:17 -0300

    all

---
+++ asteroids/circleshape.py
@@ -0,0 +1,25 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+
+    def check_collisions(self, circle):
+        return pygame.math.Vector2.distance_to(self.position, circle.position) <= (self.radius + circle.radius)
